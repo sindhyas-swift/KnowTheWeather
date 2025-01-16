8d26023cc8a698f5375f4be69aafe41b8e79cf94
Sindhya Raj <sindhyaraj@hotmail.com>
2025-01-16 12:51:23 -0500

    Initial Commit

---
+++ KnowTheWeather/KnowTheWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowTheWeatherApp.swift
+//  KnowTheWeather
+//
+//  Created by SINDHYA ANOOP on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct KnowTheWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KnowTheWeatherTests/KnowTheWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  KnowTheWeatherTests.swift
+//  KnowTheWeatherTests
+//
+//  Created by SINDHYA ANOOP on 1/16/25.
+//
+
+import Testing
+@testable import KnowTheWeather
+
+struct KnowTheWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
