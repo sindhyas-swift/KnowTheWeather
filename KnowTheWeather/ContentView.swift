b8a59f958c0e4e776c83a70616aa3813eadfee29
Sindhya Raj <sindhyaraj@hotmail.com>
2025-01-16 13:40:31 -0500

    Initial set up of background

--- KnowTheWeather/ContentView.swift
+++ KnowTheWeather/ContentView.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue,.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
         }
         .padding()
     }
