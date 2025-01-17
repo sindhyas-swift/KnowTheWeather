2529af0779e416bd0d41ba1dbb4d5278d865a6be
Sindhya Raj <sindhyaraj@hotmail.com>
2025-01-17 12:55:17 -0500

    initial screen name and temp

--- KnowTheWeather/ContentView.swift
+++ KnowTheWeather/ContentView.swift
@@ -11,7 +11,26 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue,.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .ignoresSafeArea()
+                .ignoresSafeArea(.all)
+            
+            
+            VStack{
+                Text("Cupertino CA")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 35,weight: .medium,design: .default))
+                    .padding()
+                VStack{
+                    Image(systemName: "sun.snow.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: 180,height: 180)
+                        .aspectRatio(contentMode: .fit)
+                    Text("76°")
+                        .font(.system(size: 70,weight: .medium,design: .default))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
         }
         .padding()
     }
